c68bf236a4ec338fa9bcf2a773718cf76f468b7d
NikitaPopovA <gantss12@mail.ru>
2023-03-15 23:05:39 +0300

    устраняю ошибки 5пр

---
+++ scripts/constants.js
@@ -0,0 +1,34 @@
+// Массив объектов карточек
+const elementCardMass = [
+  {
+    title: "Moscow, Russia",
+    image: "https://images.unsplash.com/photo-1580033813221-dbe4d224e1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80",
+      alt: "Moscow, Russia",
+  },
+  {
+    title: "Tokyo, Japan",
+    image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1494&q=80",
+      alt: "Tokyo, Japan",
+  },
+  {
+    title: "Toronto, Canada",
+    image: "https://images.unsplash.com/photo-1517935706615-2717063c2225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
+      alt: "Toronto, Canada",
+  },
+  {
+    title: "New York, USA",
+    image: "https://images.unsplash.com/photo-1558369178-6556d97855d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "New York, USA",
+  },
+  {
+    title: "Paris, France",
+    image: "https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "Paris, France",
+  },
+  {
+    title: "Alesund, Norway",
+    image: "https://images.unsplash.com/photo-1475066392170-59d55d96fe51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      alt: "Alesund, Norway",
+  },
+
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+/* --------------------- Переменные --------------------------------------------------------*/
+
 // Обращаюсь к popup секции edit-profile и add-card
 const profilePopup = document.querySelector('.popup_edit-profile');
 const addCardPopup = document.querySelector('.popup_add-card');
@@ -15,40 +17,6 @@ const userOccupation = document.querySelector(".profile__subtitle");
 // Поиск ввода имени и профессии
 const userNameInput = profilePopup.querySelector(".popup__input_type_name");
 const occupationInput = profilePopup.querySelector(".popup__input_type_job");
-// Массив объектов карточек
-const ElementCardMass = [
-  {
-    title: "Moscow, Russia",
-    image: "https://images.unsplash.com/photo-1580033813221-dbe4d224e1df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80",
-      alt: "Moscow, Russia",
-  },
-  {
-    title: "Tokyo, Japan",
-    image: "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1494&q=80",
-      alt: "Tokyo, Japan",
-  },
-  {
-    title: "Toronto, Canada",
-    image: "https://images.unsplash.com/photo-1517935706615-2717063c2225?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
-      alt: "Toronto, Canada",
-  },
-  {
-    title: "New York, USA",
-    image: "https://images.unsplash.com/photo-1558369178-6556d97855d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      alt: "New York, USA",
-  },
-  {
-    title: "Paris, France",
-    image: "https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      alt: "Paris, France",
-  },
-  {
-    title: "Alesund, Norway",
-    image: "https://images.unsplash.com/photo-1475066392170-59d55d96fe51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      alt: "Alesund, Norway",
-  },
-
-];
 // Контейнер который будет добавлять карточки
 const cardsContainer = document.querySelector(".elements");
 // Форма добавления карточки
@@ -67,6 +35,130 @@ const magnificationTitle = document.querySelector(".popup__magnification-title")
 const closeMagnificationBtn = magnificationPopup.querySelector(".popup__close-btn-magnification");
 
 
+
+/* --------------------- Функции --------------------------------------------------------*/
+
+// Две функции которые добавляют и удаляют popup_opened
+const openPopup = (popupElement) => popupElement.classList.add("popup_opened");
+const closePopup = (popupElement) => popupElement.classList.remove("popup_opened");
+
+// Функция которая вызывает функцию openPopup , чтобы показать всплывающее окно, потом устанавливает значение полей в этом окне
+const openProfilePopup = () => {
+  openPopup(profilePopup);
+  userNameInput.value = userName.textContent;
+  occupationInput.value = userOccupation.textContent;
+};
+
+// Функция которая обрабатывает отправку формы для редактирования профиля
+const formSubmitProfile = (evt) => {
+  evt.preventDefault();
+  userName.textContent = userNameInput.value;
+  userOccupation.textContent = occupationInput.value;
+  closePopup(profilePopup);
+};
+
+// Функция создает карточку card на основе шаблона. Функция кланирует этот шаблон и настраивает его содержимое
+const createCard = (card) => {
+  const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
+  const cardImg = cardTemplate.querySelector(".grid-card__image");
+  const cardTitle = cardTemplate.querySelector(".grid-card__title");
+  const likeBtn = cardTemplate.querySelector(".grid-card__like");
+  const deleteBtn = cardTemplate.querySelector(".grid-card__btn-delete");
+
+  cardImg.setAttribute("src", card.image);
+  cardImg.setAttribute("alt", card.alt);
+  cardTitle.textContent = card.title;
+
+  likeBtn.addEventListener("click", () => likeBtn.classList.toggle("grid-card__like_active"));
+  deleteBtn.addEventListener("click", () => deleteBtn.closest(".grid-card").remove());
+
+  cardImg.addEventListener("click", () => {
+  const title = cardTitle.textContent;
+  const alt = cardImg.getAttribute("alt");
+  magnificationImg.src = cardImg.getAttribute("src");
+  magnificationImg.alt = alt;
+  magnificationTitle.textContent = title;
+  openPopup(magnificationPopup);
+  });
+
+  return cardTemplate;
+};
+
+// Функция для добавления карточки на страницу
+const renderCard = (card, cardsContainer) => cardsContainer.prepend(createCard(card));
+elementCardMass.forEach((card) => cardsContainer.appendChild(createCard(card)));
+
+// Функция обработчик событий отправки формы для добавления новой карточки на страницу
+const handleFormSubmitCard = (evt) => {
+  evt.preventDefault();
+  const newCard = {
+    title: nameImage.value,
+    image: linkImage.value,
+    alt: nameImage.value,
+  };
+  renderCard(newCard, cardsContainer);
+  formAddCard.reset();
+  closePopup(addCardPopup);
+};
+
+
+/* --------------------- Обработчики событий  --------------------------------------------------------*/
+
+editProfileBtn.addEventListener("click", () => openProfilePopup());
+closeProfileBtn.addEventListener("click", () => closePopup(profilePopup));
+formProfile.addEventListener("submit", formSubmitProfile);
+addCardBtn.addEventListener("click", () => openPopup(addCardPopup));
+closeAddCardBtn.addEventListener("click", () => closePopup(addCardPopup));
+formAddCard.addEventListener("submit", handleFormSubmitCard);
+
+cardsContainer.addEventListener("click", (evt) => {
+  const target = evt.target;
+
+  if (target.classList.contains("grid-card__image")) {
+  const card = target.closest(".grid-card");
+  const title = card.querySelector(".grid-card__title").textContent;
+  const alt = target.getAttribute("alt");
+  magnificationImg.src = target.getAttribute("src");
+  magnificationImg.alt = alt;
+  magnificationTitle.textContent = title;
+  openPopup(magnificationPopup);
+  }
+});
+
+closeMagnificationBtn.addEventListener("click", () => closePopup(magnificationPopup));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Две функции которые добавляют и удаляют popup_opened
 const openPopup = (popupElement) => popupElement.classList.add("popup_opened");
 const closePopup = (popupElement) => popupElement.classList.remove("popup_opened");
@@ -91,22 +183,165 @@ formProfile.addEventListener("submit", formSubmitProfile);
 addCardBtn.addEventListener("click", () => openPopup(addCardPopup));
 closeAddCardBtn.addEventListener("click", () => closePopup(addCardPopup));
 
-// Функция создает карточку card на основе шаблона. Функция кланирует этот шаблон и настраивает его содержимое
+// Функция для создания карточки
 const createCard = (card) => {
   const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
   const cardImg = cardTemplate.querySelector(".grid-card__image");
   const cardTitle = cardTemplate.querySelector(".grid-card__title");
+  const likeBtn = cardTemplate.querySelector(".grid-card__like");
+  const deleteBtn = cardTemplate.querySelector(".grid-card__btn-delete");
+
   cardImg.setAttribute("src", card.image);
   cardImg.setAttribute("alt", card.alt);
   cardTitle.textContent = card.title;
+
+  // Добавляем обработчики событий на кнопки "лайк" и "удалить"
+  likeBtn.addEventListener("click", () => likeBtn.classList.toggle("grid-card__like_active"));
+  deleteBtn.addEventListener("click", () => deleteBtn.closest(".grid-card").remove());
+
+  // Добавляем обработчик события на картинку для открытия модального окна с увеличенной картинкой
+  cardImg.addEventListener("click", () => {
+  const title = cardTitle.textContent;
+  const alt = cardImg.getAttribute("alt");
+  magnificationImg.src = cardImg.getAttribute("src");
+  magnificationImg.alt = alt;
+  magnificationTitle.textContent = title;
+  openPopup(magnificationPopup);
+  });
+
+  return cardTemplate;
+  };
+
+// Функция для добавления карточки на страницу
+const renderCard = (card, cardsContainer) => cardsContainer.prepend(createCard(card));
+elementCardMass.forEach((card) => cardsContainer.appendChild(createCard(card)));
+
+
+ // Функция для обработки отправки формы для добавления новой карточки на страницу
+const handleFormSubmitCard = (evt) => {
+  evt.preventDefault();
+  const newCard = {
+    title: nameImage.value,
+    image: linkImage.value,
+    alt: nameImage.value,
+  };
+  renderCard(newCard, cardsContainer);
+  formAddCard.reset();
+  closePopup(addCardPopup);
+};
+
+
+  formAddCard.addEventListener("submit", handleFormSubmitCard);
+
+  // Добавляем обработчик событий на контейнер для обработки кликов на карточках
+  cardsContainer.addEventListener("click", (evt) => {
+  const target = evt.target;
+
+  if (target.classList.contains("grid-card__image")) {
+  const card = target.closest(".grid-card");
+  const title = card.querySelector(".grid-card__title").textContent;
+  const alt = target.getAttribute("alt");
+  magnificationImg.src = target.getAttribute("src");
+  magnificationImg.alt = alt;
+  magnificationTitle.textContent = title;
+  openPopup(magnificationPopup);
+  }
+  });
+
+  // Добавляем обработчик события на кнопку закрытия модального окна с увеличенной картинкой
+  closeMagnificationBtn.addEventListener("click", () => closePopup(magnificationPopup)); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Две функции которые добавляют и удаляют popup_opened
+const openPopup = (popupElement) => popupElement.classList.add("popup_opened");
+const closePopup = (popupElement) => popupElement.classList.remove("popup_opened");
+
+// Функция которая вызывает функцию openPopup , чтобы показать всплывающее окно, потом устанавливает значение полей в этом окне
+const openProfilePopup = () => {
+  openPopup(profilePopup);
+  userNameInput.value = userName.textContent;
+  occupationInput.value = userOccupation.textContent;
+};
+// Функция которая обрабатывает отправку формы для редактирования профиля
+const formSubmitProfile = (evt) => {
+  evt.preventDefault();
+  userName.textContent = userNameInput.value;
+  userOccupation.textContent = occupationInput.value;
+  closePopup(profilePopup);
+};
+// Обработчики событий
+editProfileBtn.addEventListener("click", () => openProfilePopup());
+closeProfileBtn.addEventListener("click", () => closePopup(profilePopup));
+formProfile.addEventListener("submit", formSubmitProfile);
+addCardBtn.addEventListener("click", () => openPopup(addCardPopup));
+closeAddCardBtn.addEventListener("click", () => closePopup(addCardPopup));
+
+const createCard = (card) => {
+  const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
+  const cardImg = cardTemplate.querySelector(".grid-card__image");
+  const cardTitle = cardTemplate.querySelector(".grid-card__title");
+  const likeButton = cardTemplate.querySelector(".grid-card__like");
+  const deleteButton = cardTemplate.querySelector(".grid-card__btn-delete");
+
+  cardImg.setAttribute("src", card.image);
+  cardImg.setAttribute("alt", card.alt);
+  cardTitle.textContent = card.title;
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("grid-card__like_active");
+  });
+
+  deleteButton.addEventListener("click", () => {
+    deleteButton.closest(".grid-card").remove();
+  });
+
+  cardImg.addEventListener("click", () => {
+    magnificationImg.src = card.image;
+    magnificationImg.alt = card.alt;
+    magnificationTitle.textContent = card.title;
+    openPopup(magnificationPopup);
+  });
+
   return cardTemplate;
 };
 
-// Функции относятся к отрисовке карточек на стронице
-const renderCard = (card, cardsContainer) => cardsContainer.prepend(createCard(card));
-ElementCardMass.forEach((card) => cardsContainer.appendChild(createCard(card)));
+const renderCard = (card, cardsContainer) => cardsContainer.appendChild(createCard(card));
 
-// Функция обработчик событий отправки формы для добавления новой карточки на страницу
+elementCardMass.forEach((card) => renderCard(card, cardsContainer));
+
+
 const handleFormSubmitCard = (evt) => {
   evt.preventDefault();
   const newCard = {
@@ -121,25 +356,6 @@ const handleFormSubmitCard = (evt) => {
 
 formAddCard.addEventListener("submit", handleFormSubmitCard);
 
-// Добавляем обработчик событий на контейнер который отвечает за обработку кликов
-cardsContainer.addEventListener("click", (evt) => {
-  const target = evt.target;
-
-  if (target.classList.contains("grid-card__like")) {
-    target.classList.toggle("grid-card__like_active");
-  }
-  else if (target.classList.contains("grid-card__btn-delete")) {
-    target.closest(".grid-card").remove();
-  }
-  else if (target.classList.contains("grid-card__image")) {
-    const card = target.closest(".grid-card");
-    const title = card.querySelector(".grid-card__title").textContent;
-    const alt = target.getAttribute("alt");
-    magnificationImg.src = target.src;
-    magnificationImg.alt = alt;
-    magnificationTitle.textContent = title;
-    openPopup(magnificationPopup);
-  }
-});
-
 closeMagnificationBtn.addEventListener("click", () => closePopup(magnificationPopup));
+
+*/
